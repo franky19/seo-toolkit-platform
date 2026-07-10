3685eeff9de4beeb93afa96d2e742142ded81647
franky19 <frankimarbun@gmail.com>
2026-07-10 15:46:29 +0700

    Revert "add navbar and footer"
    This reverts commit b8450ef68d626a6e82579ff2b3390c2e12a5a1c7.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Providers from "@/components/providers";
-import Navbar from "@/components/navbar";
-import Footer from "@/components/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,11 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar />
         <Providers>
             {children}
         </Providers>
-        <Footer />
       </body>
     </html>
   );
